de86cbf90fadda82a60290b1ca3e5c4aa466b39f
ongweisheng <ongweisheng30@gmail.com>
2022-08-30 21:43:39 +0800

    Add code for phonebook step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,21 +3,23 @@ import { useState } from 'react'
 const Person = ({ person }) => {
   return (
     <div>
-      {person.name}
+      {person.name} {person.number}
     </div>
   )
 }
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: "040-1234567" }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState("")
 
-  const addName = (event) => {
+  const addContact = (event) => {
     event.preventDefault()
     const nameObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
     if (persons.some((person) => person.name === newName)) {
       alert(`${newName} is already added to phonebook`)
@@ -32,14 +34,29 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
-        <input
+      <form onSubmit={addContact}>
+        <div>
+          name:
+          <input
           value={newName}
           onChange={handleNameChange}
-        />
+          />
+        </div>
+        <div>
+          number:
+          <input
+            value={newNumber}
+            onChange={handleNumberChange}
+          />
+        </div>
         <div>
           <button type="submit">add</button>
         </div>
