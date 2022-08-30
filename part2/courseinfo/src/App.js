2afb77d6246b3e5f522127f2482006cad62c7afe
ongweisheng <ongweisheng30@gmail.com>
2022-08-30 20:16:06 +0800

    Add code for courseinfo step 7-9 Add Course component as a separate module

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,40 +1,4 @@
-const Header = ({ course }) => <h1>{course}</h1>
-
-// const Total = ({ sum }) => <p>Number of exercises {sum}</p>
-
-const Part = ({ part }) => 
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
-const Content = ({ parts }) => 
-  <>
-    <Part
-      part={parts[0]} 
-    />
-    <Part
-      part={parts[1]} 
-    />
-    <Part
-      part={parts[2]} 
-    />      
-  </>
-
-const Course = ({ courses }) => {
-  return (
-    <>
-      {courses.map(course => {
-        return (
-          <div key={course.id}>
-            <Header course={course.name} />
-            <Content parts={course.parts} />
-            {/* <Total parts={course.parts} /> */}
-          </div>
-        )
-      })}
-    </>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const course = [
@@ -56,6 +20,27 @@ const App = () => {
           name: 'State of a component',
           exercises: 14,
           id: 3
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
         }
       ]
     }
